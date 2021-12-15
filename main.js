94a7d3507a4460ab3c766cdffa9c2920bda5bcdd
zpereiracollege <z_pereira@fanshaweonline.ca>
2021-12-14 21:11:54 -0500

    modified the main.js file by pulling the update from github and now locally pushing it back

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 (()=> {
     "use strict"
 
-    console.log("Modifying the script from github online");
+    console.log("I have been pushed to the github project");
+	
+	
+	let math = 0;
+	
+	math = 5+5;
+	
+	console.log(math);
 
 })();
